46f35ef7124f9620a9534ffb16bad6df54f035ba
yagmurbasoglu <yagmurbasoglu2003@gmail.com>
2025-07-02 17:02:34 +0300

    userservice oluşturuldu

--- userservice/src/main/java/com/PetVerse/userservice/UserserviceApplication.java
+++ userservice/src/main/java/com/PetVerse/userservice/UserserviceApplication.java
@@ -1,4 +1,4 @@
-package com.petverse.apigateway;
+package com.petverse.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,8 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class ApiGatewayApplication {
+public class UserserviceApplication {
     public static void main(String[] args) {
-        SpringApplication.run(ApiGatewayApplication.class, args);
-	}
+        SpringApplication.run(UserserviceApplication.class, args);
+    }
 }
