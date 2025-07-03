344711b5919144dff5fa052c1bf59014b9b55849
Sedanur Ayhan <sedanurayhan934@gmail.com>
2025-07-03 08:52:34 +0300

    petservice oluşturuldu, güncelleme yapılacak.

---
+++ petservice/src/test/java/com/petservice/petservice/PetserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.petservice.petservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
