f4c91238c3acb5c8c39411e1fee34f4acfc60259
yagmurbasoglu <yagmurbasoglu2003@gmail.com>
2025-07-15 21:03:32 +0300

    mikroservisler için health check endpointleri eklendi, tüm mikroservisleri yerel ortamda sırayla başlatmak için start-all.bat betiği eklendi.

--- userservice/src/main/java/com/PetVerse/userservice/config/SecurityConfig.java
+++ userservice/src/main/java/com/PetVerse/userservice/config/SecurityConfig.java
@@ -29,7 +29,7 @@ public class SecurityConfig {
         return http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                    .requestMatchers(
+                    .requestMatchers("/actuator/**",
                             "/api/auth/register",
                             "/api/auth/login",
                             "/swagger-ui/**",
