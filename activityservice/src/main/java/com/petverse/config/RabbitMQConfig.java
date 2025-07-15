618e813a72e6946086b17bb02b3c225e1c6afb7c
Sedanur Ayhan <sedanurayhan934@gmail.com>
2025-07-15 15:14:42 +0300

    Kullanıcı token’ına göre sadece endi bildirimlerini görüntüleyebileceği şekilde yetkilendirme eklendi.

--- activityservice/src/main/java/com/petverse/config/RabbitMQConfig.java
+++ activityservice/src/main/java/com/petverse/config/RabbitMQConfig.java
@@ -1,6 +1,9 @@
 package com.petverse.config;
 
 import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -24,5 +27,19 @@ public class RabbitMQConfig {
     @Bean
     public Binding binding(Queue queue, TopicExchange exchange) {
         return BindingBuilder.bind(queue).to(exchange).with(ROUTING_KEY);
+    }
+
+    // JSON mesaj converter
+    @Bean
+    public Jackson2JsonMessageConverter messageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory,
+                                         Jackson2JsonMessageConverter messageConverter) {
+        RabbitTemplate template = new RabbitTemplate(connectionFactory);
+        template.setMessageConverter(messageConverter);
+        return template;
     }
 }
