2e17a11c958caa522e549d3c38bc28ec1bc8ce6c
yagmurbasoglu <yagmurbasoglu2003@gmail.com>
2025-07-22 11:42:54 +0300

    PT-3: ActivityService contextLoads test removed to fix CI build failure

--- activityservice/src/test/java/com/petverse/activityservice/ActivityserviceApplicationTests.java
+++ activityservice/src/test/java/com/petverse/activityservice/ActivityserviceApplicationTests.java
@@ -2,18 +2,30 @@ package com.petverse.activityservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
+@Testcontainers
+@SpringBootTest
+public class ActivityserviceApplicationTests {
 
-@SpringBootTest(properties = {
-  "spring.datasource.url=jdbc:postgresql://localhost:5432/petcaredb",
-  "spring.datasource.username=admin",
-  "spring.datasource.password=admin123",
-  "spring.jpa.hibernate.ddl-auto=none"
-})
-class ActivityserviceApplicationTests {
+    @Container
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:15")
+            .withDatabaseName("petcaredb")
+            .withUsername("admin")
+            .withPassword("admin123");
 
-	@Test
-	void contextLoads() {
-	}
+    @DynamicPropertySource
+    static void overrideProps(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
+    }
 
+    @Test
+    void contextLoads() {
+    }
 }
