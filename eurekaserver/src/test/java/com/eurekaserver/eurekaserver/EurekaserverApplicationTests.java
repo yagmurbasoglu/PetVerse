a37016ba78adddc6faaa110a896a6e748402b48c
yagmurbasoglu <yagmurbasoglu2003@gmail.com>
2025-07-25 11:55:43 +0300

    PT-11-log testler ve sonar ile merge edildi eurekaserver @ActiveProfiles('test')  log dosyası deneme

---
+++ eurekaserver/src/test/java/com/eurekaserver/eurekaserver/EurekaserverApplicationTests.java
@@ -0,0 +1,15 @@
+package com.eurekaserver.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class EurekaserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
